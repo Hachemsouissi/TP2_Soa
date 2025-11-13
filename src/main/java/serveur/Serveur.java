205c1d403a4b57615a8ec0201a29b6fce81e93af
Hachem <hachemsouissi1@gmail.com>
2025-11-13 22:32:22 +0100

    3

--- src/main/java/serveur/Serveur.java
+++ src/main/java/serveur/Serveur.java
@@ -5,8 +5,6 @@ import service.Calculatrice;
 public class Serveur {
 
     public static void main(String[] args) {
-        // L'URL où notre service sera accessible.
-        // Vous pouvez choisir un autre port si le 8080 est déjà utilisé.
         String url = "http://localhost:8080/calculatrice";
         Endpoint.publish(url, new Calculatrice());
         System.out.println("Service web Calculatrice démarré sur l'adresse : " + url);
